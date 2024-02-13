e4ed96c33443f7c39537783ec73e9b4c09c82c79
Mateusz Rojek <mateuszr2047@gmail.com>
2024-02-13 20:50:04 +0100

    Utworzenie projektu

---
+++ src/main/java/pl/matipol/listadodawanieuzytkownikow/ListaDodawanieUzytkownikowApplication.java
@@ -0,0 +1,13 @@
+package pl.matipol.listadodawanieuzytkownikow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDodawanieUzytkownikowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaDodawanieUzytkownikowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/matipol/listadodawanieuzytkownikow/ListaDodawanieUzytkownikowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matipol.listadodawanieuzytkownikow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDodawanieUzytkownikowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
